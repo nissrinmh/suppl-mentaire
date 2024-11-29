04d9e0bda1c9cb99ee5343b49a5ea7436ad8c5a8
nissrinmh <nissrinmahan02@gmail.com>
2024-11-29 18:32:25 +0100

    Update carte.jsx

--- src/components/body/carte.jsx
+++ src/components/body/carte.jsx
@@ -12,7 +12,7 @@ const Body=()=>{
         <div class="card-body">
           <h5 class="card-title">Wirless bluetooth headset</h5>
           <p class="card-text">Shipped in 3-4 days</p>
-          <p>$35.99</p>
+          <p className='prix'>$35.99</p>
           <a href=" " class="btn " id='but2'> <img src={cart} className='cart' alt=""/> Add Cart</a>
           <a href=" " class="btn" id='but1' ><img src={buy} className='cart' alt=""/> Buy</a>
         </div>
